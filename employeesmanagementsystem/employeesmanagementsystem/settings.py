054bacde20602e3154e549fdd89ce93e197e1a23
Ganontha <bganontha@gmail.com>
2024-12-19 16:33:26 +0100

    Setup for mysql database from the hosting provider

--- employeesmanagementsystem/employeesmanagementsystem/settings.py
+++ employeesmanagementsystem/employeesmanagementsystem/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'employeesmanagementsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Bellehouse$employees_db',
+        'USER': 'Bellehouse',
+        'PASSWORD': 'empLoyeesDB$100$',
+        'HOST': 'Bellehouse.mysql.pythonanywhere-services.com',  # e.g., 'yourusername.mysql.pythonanywhere-services.com'
+        'PORT': '3306',
     }
 }
 
