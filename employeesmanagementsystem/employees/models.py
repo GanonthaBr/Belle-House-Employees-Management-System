042996f17387a4fc1c1f09f3c13c487ba323e3fc
Ganontha <bganontha@gmail.com>
2024-12-19 14:09:06 +0100

    Refactor Employee model and migrations to use unique employee_id and adjust primary key

--- employeesmanagementsystem/employees/models.py
+++ employeesmanagementsystem/employees/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    employee_id = models.AutoField(primary_key=True)
+    id  = models.AutoField(primary_key=True)
+    employee_id = models.CharField(max_length=16, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
