2b7c03e90404a6b7053cc7bc0e9caa5d41068fc3
Ganontha <bganontha@gmail.com>
2025-01-31 16:59:23 +0100

    fix: re-add corsheaders to INSTALLED_APPS in settings.py

--- employeesmanagementsystem/employeesmanagement/settings.py
+++ employeesmanagementsystem/employeesmanagement/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'employees',
-    'corsheaders',
     'django_filters',
     'wageslip',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
