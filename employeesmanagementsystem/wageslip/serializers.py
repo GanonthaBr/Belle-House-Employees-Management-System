f60aa3c10e09383dc2b0740ee39beb50e7e994d8
Ganontha <bganontha@gmail.com>
2025-03-28 11:09:41 +0100

    Implement custom update method in InvoiceSerializer to handle designations

--- employeesmanagementsystem/wageslip/serializers.py
+++ employeesmanagementsystem/wageslip/serializers.py
@@ -28,7 +28,27 @@ class InvoiceSerializer(ModelSerializer):
     class Meta:
         model = Invoice
         fields = ['id','topic','name','date','number','echeance','client','client_id','tax','type_tax','payment_mode','designations','montant_avance','total_amount','total','stamp']
-    
+        read_only_fields = ['total_amount','total']
+
+    #explicitly define update method
+    def update(self,instance,validated_data):
+        designation_data = validated_data.pop('designations')
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if designation_data:
+            for designation in designation_data:
+                designation_id = designation.get('id',None)
+                if designation_id:
+                    designation_obj = Designation.objects.get(id=designation_id,invoice=instance)
+                    for attr, value in designation.items():
+                        setattr(designation_obj,attr,value)
+                        designation_obj.save()
+                else:
+                    designation_obj = Designation.objects.create(invoice=instance,**designation)
+
+
     def get_total(self,obj):
         return obj.total
     def get_total_amount(self,obj):
