f87ed195e48dc33d22b680df2aaf37c323be1a48
Ganontha <bganontha@gmail.com>
2025-04-07 16:53:53 +0100

    Refactor employee views and URL routing for improved API structure

--- employeesmanagementsystem/employees/urls.py
+++ employeesmanagementsystem/employees/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
-from .views import EmployeeViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'employees',EmployeeViewSet,basename='employees')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('employees/<int:pk>', views.EmployeeView.as_view()),
+    path('employees/', views.EmployeeList.as_view()),
+    path('employees/<int:pk>', views.EmployeeView.as_view()),
 ]
--- employeesmanagementsystem/employees/views.py
+++ employeesmanagementsystem/employees/views.py
@@ -4,22 +4,10 @@ from .models import Employee
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 
-class EmployeeViewSet(ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-
-    # def get_object(self):
-    #     # Fetch the custom field value from the URL
-    #     lookup_field_value = self.kwargs.get('employee_id') 
-    #     try:
-    #         return Employee.objects.get(employee_id=lookup_field_value)
-    #     except:
-    #         raise Response({"error": "Employee not found!"}, status=status.HTTP_404_NOT_FOUND)
-
+class EmployeeView(APIView):
     def get(self, request, pk):
         employee = Employee.objects.get(employee_id=pk)
         if employee:
@@ -27,31 +15,31 @@ class EmployeeViewSet(ModelViewSet):
             return Response(serializer.data,status=status.HTTP_200_OK)
         return Response({"message":"Employee not found!"},status=status.HTTP_404_NOT_FOUND)
     
-    # def post(self,request):
-    #     serializer = EmployeeSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self,request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    # def put(self, request, pk):
-    #     employee = Employee.objects.get(pk=pk)
-    #     if employee:
-    #         data = request.data.copy()
-    #         data.pop('employee_id', None)  # Exclude employee_id from the update
-    #         serializer = EmployeeSerializer(employee, data=data, partial=True)
-    #         if serializer.is_valid():
-    #             serializer.save()
-    #             return Response(serializer.data, status=status.HTTP_200_OK)
-    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #     return Response({"message":"Employee not found!"},status=status.HTTP_404_NOT_FOUND)
+    def put(self, request, pk):
+        employee = Employee.objects.get(pk=pk)
+        if employee:
+            data = request.data.copy()
+            data.pop('employee_id', None)  # Exclude employee_id from the update
+            serializer = EmployeeSerializer(employee, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"Employee not found!"},status=status.HTTP_404_NOT_FOUND)
     
-    # def delete(self, request, pk):
-    #     employee = Employee.objects.get(pk=pk)
-    #     if employee:
-    #         employee.delete()
-    #         return Response({"message":"Deleted!"},status=status.HTTP_204_NO_CONTENT)
-    #     return Response({"message":"Employee not found!"},status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, pk):
+        employee = Employee.objects.get(pk=pk)
+        if employee:
+            employee.delete()
+            return Response({"message":"Deleted!"},status=status.HTTP_204_NO_CONTENT)
+        return Response({"message":"Employee not found!"},status=status.HTTP_404_NOT_FOUND)
     
 
 class EmployeeList(APIView):
