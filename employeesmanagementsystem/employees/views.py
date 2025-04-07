2b7abec5f4e34034d0d67c9c6ee8ed5323f036fa
Ganontha <bganontha@gmail.com>
2025-04-07 16:55:57 +0100

    Refactor employee views and URL routing for improved API structure

--- employeesmanagementsystem/employees/views.py
+++ employeesmanagementsystem/employees/views.py
@@ -15,13 +15,7 @@ class EmployeeView(APIView):
             return Response(serializer.data,status=status.HTTP_200_OK)
         return Response({"message":"Employee not found!"},status=status.HTTP_404_NOT_FOUND)
     
-    def post(self,request):
-        serializer = EmployeeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+   
     def put(self, request, pk):
         employee = Employee.objects.get(pk=pk)
         if employee:
@@ -47,3 +41,11 @@ class EmployeeList(APIView):
         employees = Employee.objects.all()
         serializer = EmployeeSerializer(employees, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+     
+     def post(self,request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
